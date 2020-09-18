c03df63fc8b6d0a377b6dd3cf2018019ebff8e38
Mychael M <mychaelbmenges@gmail.com>
2020-09-17 19:01:01 -0600

    created form structure with fields for Login and Signup forms

--- silent-auction-fe/src/components/login.js
+++ silent-auction-fe/src/components/login.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 const LoginForm = (props) => {
-  return(
+  return (
     <>
-    <h1>This is the login page</h1>
-    <form>
-      <input 
-      type="text"
-      name="username"
-      id="username"/>
-    </form>
+      <h1>This is the login page</h1>
+      <form>
+        <label htmlFor="username">
+          <input 
+          type="text" 
+          name="username" 
+          id="username" 
+          placeholder="Username" />
+        </label>
+        <br/>
+
+        <label htmlFor="password">
+          <input 
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"/>
+        </label>
+        <br/>
+
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 }
--- silent-auction-fe/src/components/signup.js
+++ silent-auction-fe/src/components/signup.js
@@ -2,7 +2,51 @@ import React from 'react';
 
 const SignupForm = (props) => {
   return (
-    <h1>This is the Signup page</h1>
+    <>
+      <h1>This is the Signup page</h1>
+      <form>
+        <label htmlFor="username">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+          />
+        </label>
+        <br />
+
+        <label htmlFor="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </label>
+        <br />
+
+        <label htmlFor="name">
+          <input 
+          type="text" 
+          name="name" 
+          id="name" 
+          placeholder="Name"/>
+        </label>
+        <br/>
+
+        <label htmlFor="email">
+          <input 
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"/>
+        </label>
+        <br/>
+        
+        <button type="submit">Sign Up</button>
+        <button>Login</button>
+      </form>
+    </>
   );
 }
 
