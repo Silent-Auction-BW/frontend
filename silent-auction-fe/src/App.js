a2bfab29ba74413cb4876ba83f7a4ae951627a68
Mychael M <mychaelbmenges@gmail.com>
2020-09-21 19:55:08 -0600

    added box shadow to header

--- silent-auction-fe/src/App.js
+++ silent-auction-fe/src/App.js
@@ -13,6 +13,7 @@ import UpdateForm from "./components/UpdateForm";
 import styled from'styled-components';
 
 const NavBar = styled.header`
+  box-shadow: 0 5px 10px black;
   display: flex;
   justify-content: center;
   background-color: #19647e;
