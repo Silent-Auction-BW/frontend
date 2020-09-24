3f02e43f45c25f0bbe3bb34cad4e98a380e72ca1
weng7533 <weng7533@users.noreply.github.com>
2020-09-23 21:20:53 -0400

    sign in fixed

--- silent-auction-fe/src/components/login.js
+++ silent-auction-fe/src/components/login.js
@@ -71,14 +71,14 @@ const formSchema = yup.object().shape({
 
 const LoginForm = (props) => {
 
-  // this useEffect is a helpper so we don't have to login. You can delete it.
-  useEffect(() => {
-    localStorage.setItem('token', 'efeijife-fefeife-fefe');
-    history.push('/SellerCard');
-    Prop.loginStateSetter(true);
-  }, [])
+  // // this useEffect is a helpper so we don't have to login. You can delete it.
+  // useEffect(() => {
+  //   localStorage.setItem('token', 'efeijife-fefeife-fefe');
+  //   history.push('/SellerCard');
+  //   Prop.loginStateSetter(true);
+  // }, [])
 
-  // this useEffect is a helpper so we don't have to login. You can delete it.
+  // // this useEffect is a helpper so we don't have to login. You can delete it.
 
 
 
@@ -139,11 +139,11 @@ const LoginForm = (props) => {
       .post(`https://bw-silent-auction-pt.herokuapp.com/login`, loginData)
 
       .then(res => {
-        console.log(res);
+        console.log('seccuss login', res);
         localStorage.setItem('token', 'efeijife-fefeife-fefe');
         Prop.loginStateSetter(true);
         console.log('props', props)
-        res.data.message.includes("seller") === true
+        res.data.seller_id
           ?
           props.history.push('/SellerCard')
           :
